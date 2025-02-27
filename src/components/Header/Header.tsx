c168b378cbe4fd62ed023c4bbab9ec3d6f4b9af2
wabemusic <jvwatanabe@gmail.com>
2025-02-26 22:16:51 -0300

    adding components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <img src="#" alt="Circuit Coffee logo" />
+        {/* Cart component */}
+      </header>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from '../$node_modules/@types/react/index.js'
-import { createRoot } from '../$node_modules/@types/react-dom/client.js'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Header } from "./components/Header/Header";
+// import './index.css'
+// import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header/>
   </StrictMode>,
 )
