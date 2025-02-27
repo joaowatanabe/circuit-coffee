73703eb5d43b2fd940df67588094edb24681f9b0
wabemusic <jvwatanabe@gmail.com>
2025-02-26 22:19:46 -0300

    feat: Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,39 @@
-export const Footer = () => {
-  return <></>;
-};
+export function Footer() {
+  return (
+    <footer className="footer">
+      <img src="#" alt="Circuit coffee logo" />
+      <div className="container-products-footer">
+        <div className="products-footer">
+          <h3>Classicos</h3>
+          <ul>
+            <li>Expresso</li>
+            <li>Americano</li>
+            <li>Capuccino</li>
+            <li>Latte</li>
+            <li>Macchiato</li>
+            <li>Mocha</li>
+            <li>Irish Coffee</li>
+          </ul>
+        </div>
+        <div className="products-footer">
+          <h3>Gelados</h3>
+          <ul>
+            <li>Cold Brew</li>
+            <li>Frappuccino</li>
+            <li>Laranja & Tônica</li>
+            <li>Milk shake</li>
+          </ul>
+        </div>
+        <div className="products-footer">
+          <h3>Veganos</h3>
+          <ul>
+            <li>Expresso</li>
+            <li>Capuccino Veg</li>
+            <li>Cold Brew</li>
+            <li>Frappuccino Veg</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
