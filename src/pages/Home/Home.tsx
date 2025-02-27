9b4f4e7e0a448cc2b35a6ad567703e417690f63c
wabemusic <jvwatanabe@gmail.com>
2025-02-26 22:21:06 -0300

    feat: Pages Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,10 @@
-import { IProduct } from "../../@types/products";
-import { listarProdutos } from "../../Api/axios";
-import PlantImage from "../../assets/Plant.png";
-import Cow from "../../assets/Cow.png";
+// import { IProduct } from "../../@types/products";
+// import { listarProdutos } from "../../Api/axios";
 import style from "./style.module.css";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { formatarPreco } from "../../utils/FormatPrice";
+// import { Link } from "react-router-dom";
+// import { formatarPreco } from "../../utils/FormatPrice";
+import { Products } from "../../components/Products/Products";
 
 export function Home() {
   const [produtos, setProdutos] = useState<IProduct[]>([]);
@@ -24,85 +23,30 @@ export function Home() {
   }, []);
 
   return (
-    <main>
+    <main className="main-page">
       <section className="slogan">
-        <div className="container">
+        <div className="container-slogan">
           <h1 className={style["slogan__title"]}>
-            Fragrância e sabor elevado ao cubo
+            A Energia do café que deixa no ritmo certo
           </h1>
+        </div>
+        <div className="container-slogan__text">
           <p className="slogan__text">
-            Explore nossa variedade de blends exclusivos e saboreie a perfeição
-            em cada gole.
+            Trabalhamos com o conceito de coffee experience. Apresentamos cafés
+            raros e exóticos com sensoriais muito nítidos. Fazendo o blend
+            perfeito com musicas energéticas.
           </p>
         </div>
       </section>
       <section className={style["products"]}>
-        <div className="container">
+        <div className="container-classicos">
           <h2 className={style["products__title"]}>Clássicos</h2>
-          <div className={style["products__list"]}>
-            {produtos
-              .filter((item) => item.categoria === "classicos")
-              .map((produto) => {
-                return (
-                  <Link
-                    key={produto.id}
-                    to={`/product/${produto.id}`}
-                    className={style["products__list--item"]}
-                  >
-                    <img src={`../${produto.imagem}`} alt="" />
-                    <h3 className={style["products__list--price"]}>
-                      {formatarPreco(produto.preco.por)}{" "}
-                      <span>{formatarPreco(produto.preco.de)}</span>
-                    </h3>
-                    <h4 className={style["products__list--name"]}>
-                      {produto.nome}
-                    </h4>
-                    {produto.vegano ? (
-                      <div className="product__tag">
-                        <img src={PlantImage} alt="planta" />
-                        <span>Vegano</span>
-                      </div>
-                    ) : (
-                      <div className="product__tag">
-                        <img src={Cow} alt="Vaca" />
-                        <span>Contém Lactose</span>
-                      </div>
-                    )}
-                  </Link>
-                );
-              })}
-          </div>
+          <Products />
         </div>
       </section>
       <section className={style["products"]}>
-        <div className="container">
+        <div className="container-gelados">
           <h2 className={style["products__title"]}>Gelados</h2>
-          <div className={style["products__list"]}>
-            {produtos
-              .filter((item) => item.categoria === "gelados")
-              .map((produto) => {
-                return (
-                  <Link
-                    key={produto.id}
-                    to={`/product/${produto.id}`}
-                    className={style["products__list--item"]}
-                  >
-                    <img src={`../${produto.imagem}`} alt="" />
-                    <h3 className={style["products__list--price"]}>
-                      {formatarPreco(produto.preco.por)}{" "}
-                      <span>{formatarPreco(produto.preco.de)}</span>
-                    </h3>
-                    <h4 className={style["products__list--name"]}>
-                      {produto.nome}
-                    </h4>
-                    <div className="product_tag">
-                      <img src={PlantImage} alt="planta" />
-                      <span>Vegano</span>
-                    </div>
-                  </Link>
-                );
-              })}
-          </div>
         </div>
       </section>
     </main>
